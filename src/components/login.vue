83e6dcf8e03620da88827008a2d269414124c036
Natanael Silva <natanael.silva@mv.com.br>
2025-03-25 09:11:45 -0300

    feat: adiciona componente de login e estilos

---
+++ src/components/login.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <form action="" class="login">
+    <div class="login__group">
+      <label for="username">Usuário</label>
+      <input type="text">
+    </div>
+    <div class="login__group">
+      <label for="username">Usuário</label>
+      <input type="text">
+    </div>
+    <button type="submit" class="login__button">
+      Enviar
+    </button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
